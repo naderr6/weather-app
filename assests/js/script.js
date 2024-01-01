c6cf3862a14c3a146160d7ec01950454f8b1af10
Nader <ndrsad199@gmail.com>
2024-01-01 11:24:14 +0400

    f commit

---
+++ assests/js/script.js
@@ -0,0 +1,27 @@
+document.getElementById("weatherForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const apiKey = 'a1851c4da8d446e1ad8f281513a55183';
+    const city = document.getElementById("cityInput").value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            document.getElementById("weatherInfo").innerHTML = 'Error fetching weather data. Please try again.';
+        });
+});
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById("weatherInfo");
+    weatherInfo.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
